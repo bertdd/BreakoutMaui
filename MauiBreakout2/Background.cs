5a387d26d8867676d20b36b0d51e5fc708a11f19
EmreCop <99261888+EmreCop@users.noreply.github.com>
2023-03-23 11:18:32 +0100

    added a single cloudcollision

--- MauiBreakout2/Background.cs
+++ MauiBreakout2/Background.cs
@@ -1,10 +1,36 @@
 ﻿namespace MauiBreakout2;
 
+class backgroundchange { }
 internal class Background : Image
 {
-  public Background()
-  {
-    Source = "background.jpg";
-    Aspect = Aspect.AspectFill;
-  }
+    public Background()
+    {
+        Source = "background.jpg";
+        Aspect = Aspect.AspectFill;
+        Opacity = 1;
+    }
+    internal void Tick()
+    {
+        if (Opacity > 0)
+        {
+            Opacity -= 0.001;
+        }
+    }
+}
+
+internal class Backgroundnight : Image
+{
+    public Backgroundnight()
+    {
+        Source = "background2.jpg";
+        Aspect = Aspect.AspectFill;
+        Opacity = 0;
+    }
+    internal void Tick()
+    {
+        if (Opacity < 1)
+        {
+            Opacity += 0.001;
+        }
+    }
 }
