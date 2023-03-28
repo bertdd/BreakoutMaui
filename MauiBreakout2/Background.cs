eae93d70300a8e79f62bf047d2a41250236a5648
Aboubakr21 <aboubakr_elhadiyen@live.nl>
2023-03-28 16:57:24 +0200

    Update Background.cs

--- MauiBreakout2/Background.cs
+++ MauiBreakout2/Background.cs
@@ -1,10 +1,36 @@
-﻿namespace MauiBreakout2;
-
-internal class Background : Image
+﻿namespace MauiBreakout2
 {
-  public Background()
-  {
-    Source = "841629.jpg";
-    Aspect = Aspect.AspectFill;
-  }
+    internal class Background : Image
+    {
+        private readonly string[] imageSources = { "background.jpg", "background2.jpg" };
+        private int currentImageIndex = 0;
+
+        [Obsolete]
+        public Background()
+        {
+            Source = imageSources[currentImageIndex];
+            Aspect = Aspect.AspectFill;
+
+            Device.StartTimer(TimeSpan.FromSeconds(5), () =>
+            {
+                currentImageIndex = (currentImageIndex + 1) % imageSources.Length;
+                var nextImageSource = imageSources[currentImageIndex];
+
+                if (nextImageSource == "background2.jpg")
+                {
+                    this.FadeTo(0, 1000, Easing.Linear).ContinueWith((t) =>
+                    {
+                        Source = nextImageSource;
+                        this.FadeTo(1, 5000, Easing.Linear);
+                    });
+                }
+                else
+                {
+                    Source = nextImageSource;
+                }
+
+                return true;
+            });
+        }
+    }
 }
