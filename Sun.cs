93d626fee1bf204ee67c9872a9128cd3031811e1
zoegattino <106600443+zoegattino@users.noreply.github.com>
2023-03-29 16:27:10 +0200

    Add files via upload

---
+++ Sun.cs
@@ -0,0 +1,36 @@
+﻿namespace MauiBreakout2;
+
+internal class Sun : Image
+{
+    private bool isMovingUp = true;
+    private double stepSize = 10; 
+
+    public Sun()
+    {
+        Source = "sun.png";
+        HorizontalOptions = LayoutOptions.Center;
+
+        Device.StartTimer(TimeSpan.FromMilliseconds(50), () =>
+        {
+            if (isMovingUp)
+            {
+                TranslationY -= stepSize;
+            }
+            else
+            {
+                TranslationY += stepSize;
+            }
+
+            if (TranslationY <= -20)
+            {
+                isMovingUp = false;
+            }
+            else if (TranslationY >= 500)
+            {
+                isMovingUp = true;
+            }
+
+            return true;
+        });
+    }
+}
