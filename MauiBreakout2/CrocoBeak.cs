be7693dee67176efc60c1a1daf6345f63f4d3fc8
Opariin <akif.golge@gmail.com>
2023-04-03 22:00:29 +0200

    unedited version

---
+++ MauiBreakout2/CrocoBeak.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBreakout2;
+
+internal class CrocoBeak : GameObject
+{
+    public CrocoBeak()
+    {
+        Source = "crocobreak.png";
+        HorizontalOptions = LayoutOptions.Center;
+    }
+}
